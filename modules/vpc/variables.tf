c3a2e931e4648bfc937f89ad60900c94a73f9edd
dansarpong <dansarpong.me@gmail.com>
2025-04-16 11:40:52 +0000

    Remove default security group and related variables from VPC module

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -13,15 +13,3 @@ variable "cidr_block" {
   description = "Base CIDR block for the VPC"
   type        = string
 }
-
-variable "allowed_ssh_cidr" {
-  description = "CIDR block allowed for SSH access"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "allowed_http_cidr" {
-  description = "CIDR block allowed for HTTP/HTTPS access"
-  type        = string
-  default     = "0.0.0.0/0"
-}
